139445fddd1191eb65c5a496166c9984c3245ca8
akbastx <akbastx@gmail.com>
2020-08-08 12:46:32 -0500

    my local osman branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from the local repo");
 		System.out.println("Our second commit from the local repo");
+		System.out.println("Now I amon Osman branch");
 	}
 
 }
